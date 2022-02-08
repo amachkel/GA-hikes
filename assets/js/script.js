32b5539b0983631a8c15fde24f514e30d12bef90
unknown <pearsenation@gmail.com>
2022-02-07 20:04:46 -0500

    adding some basic event listeners and vars for hiking difficulty if we end up using them. Event listeners will need to be matched up with the correct tag in html once it's ready.

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/* GA - Hikes JS */
+var dropMenuBtn = document.querySelector('dropMenuBtn');
+
+var AndersonvilleHike = '';
+var AppalachianHike = '';
+var ChattahoocheeHike = '';
+var ChickamaugaHike = '';
+var CumberlandHike = '';
+var FortFredericaHike = '';
+var FortPulaskiHike = '';
+var KennesawHike = '';
+var JCHike = '';
+var MLKHike = '';
+var OcmulgeeHike = '';
