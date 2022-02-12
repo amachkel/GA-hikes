7cbafa0edc4a956cd92a83d4b497b549e7e5e370
amachkel <amachkel@gmail.com>
2022-02-12 11:17:02 -0500

    fixed script tag in index.html and edited click event to take user to second page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 /* GA - Hikes JS */
-var searchBtn = document.getElementById('searchBtn');
+var searchBtn = document.getElementById("searchBtn");
+searchBtn.addEventListener("click", searchFunc);
 
-if (searchBtn){
-    searchBtn.addEventListener('click', searchFunc);
+function searchFunc() {
+  console.log("Search has been clicked.");
+  window.location.replace("index2.html");
 }
 
-function searchFunc(){
-    console.log('Search has been clicked.');
-    window.location.replace("index2.html");
-}
-
-var AndersonvilleHike = ''; // These need to be changed into an Array with the associated areas.
-var AppalachianHike = '';   // The goal is to list a range of hoke lengths with the arrays and a string set up.
-var ChattahoocheeHike = ''; // It will be attached as a child element at the bottom of index2.html
+var AndersonvilleHike = ""; // These need to be changed into an Array with the associated areas.
+var AppalachianHike = ""; // The goal is to list a range of hoke lengths with the arrays and a string set up.
+var ChattahoocheeHike = ""; // It will be attached as a child element at the bottom of index2.html
 // ... list continues ...
+
+//localStorage
+
+function getSearchInput() {}
