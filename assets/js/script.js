d93af2df2104e74936f71440c6767affc25ec137
amachkel <amachkel@gmail.com>
2022-02-12 13:41:56 -0500

    targeted localStorage value with searchFunc

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,34 @@ if (searchBtn) {
   // Makes sure that the button has loaded before becoming clickable.
   searchBtn.addEventListener("click", searchFunc);
 }
+  
+
+
+    var searchEl = document.getElementById("selectPark");
+    
+    if (searchEl.matches("option") === true) {
+      var optionText = searchEl.value;
+      console.log(optionText);
+      // getSearchInput(optionText);
+      searchFunc();
+    }
 
 function searchFunc() {
+    var searchItem = searchEl.value;
+    
+    console.log(searchItem);
   // Moves user out to index2.html when the search button is clicked.
   console.log("Search has been clicked.");
-  let optionVal = this.getElementsByClassName("search");
-    // .getAttribute("value");
-  //   let searchValue = optionEl.getAttribute("value");
-  console.log(optionVal);
-  getSearchInput(optionVal);
+
+  document.getElementById;
+
+  getSearchInput(searchItem);
   window.location.replace("index2.html");
 }
 
-var AndersonvilleHike = ""; // These need to be changed into an Array with the associated areas.
-var AppalachianHike = ""; // The goal is to list a range of hoke lengths with the arrays and a string set up.
-var ChattahoocheeHike = ""; // It will be attached as a child element at the bottom of index2.html
-// ... list continues ...
-
 //localStorage
 
-function getSearchInput(e, optionVal) {
-    e.preventDefault();
-    console.log(optionVal);
+function getSearchInput(searchItem) {
+    // e.preventDefault();
+    console.log(searchItem);
 }
