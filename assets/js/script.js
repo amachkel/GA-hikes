ba763e17bef465fc37ca7b6503c2f0ec0800fb4e
David Ethridge <67756256+PearseNation@users.noreply.github.com>
2022-02-12 11:42:54 -0500

    David feature (#29)
    * added an exact copy of index to move it outside of the assets folder
    
    * adding index2 to the proper spot.
    
    * removing the files that were place in that assets folder.
    
    * corrected the file paths for css and js
    
    * adding button basic functioning
    
    * fixed the linkng to the js file
    
    * added some ideas for the child pinning element
    
    * These merges are taking some getting used to
    
    * finished adding hiking data
    
    * added a back button to index2 and set the functionality in search-results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 /* GA - Hikes JS */
-var searchBtn = document.getElementById('searchBtn');
+var searchBtn = document.getElementById('searchBtn'); // Finds the id: searchBtn to add functionality to it below.
 
-if (searchBtn){
+if (searchBtn){ // Makes sure that the button has loaded before becoming clickable.
     searchBtn.addEventListener('click', searchFunc);
 }
 
-function searchFunc(){
+function searchFunc(){ // Moves user out to index2.html when the search button is clicked.
     console.log('Search has been clicked.');
     window.location.replace("index2.html");
 }
 
-var AndersonvilleHike = ''; // These need to be changed into an Array with the associated areas.
-var AppalachianHike = '';   // The goal is to list a range of hoke lengths with the arrays and a string set up.
-var ChattahoocheeHike = ''; // It will be attached as a child element at the bottom of index2.html
-// ... list continues ...
