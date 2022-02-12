5aa8f71c87fbf765e611100554d536934311d732
Alex Harkins <amachkel@gmail.com>
2022-02-12 13:43:32 -0500

    Alex feature (#30)
    * temporarily commented out NPS data until function wrapper is fixed in merge. Removed OpenLayers map code.
    
    * Rendered forecast data to index2.
    
    * edited date display to only show calendar date
    
    * commit
    
    * fixed script tag in index.html and edited click event to take user to second page
    
    * In process of targeting localStorage value.
    
    * targeted localStorage value with searchFunc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,37 @@
-/* GA - Hikes JS */
-var searchBtn = document.getElementById('searchBtn'); // Finds the id: searchBtn to add functionality to it below.
+var searchBtn = document.getElementById("searchBtn"); // Finds the id: searchBtn to add functionality to it below.
 
-if (searchBtn){ // Makes sure that the button has loaded before becoming clickable.
-    searchBtn.addEventListener('click', searchFunc);
+if (searchBtn) {
+  // Makes sure that the button has loaded before becoming clickable.
+  searchBtn.addEventListener("click", searchFunc);
+}
+  
+
+
+    var searchEl = document.getElementById("selectPark");
+    
+    if (searchEl.matches("option") === true) {
+      var optionText = searchEl.value;
+      console.log(optionText);
+      // getSearchInput(optionText);
+      searchFunc();
+    }
+
+function searchFunc() {
+    var searchItem = searchEl.value;
+    
+    console.log(searchItem);
+  // Moves user out to index2.html when the search button is clicked.
+  console.log("Search has been clicked.");
+
+  document.getElementById;
+
+  getSearchInput(searchItem);
+  window.location.replace("index2.html");
 }
 
-function searchFunc(){ // Moves user out to index2.html when the search button is clicked.
-    console.log('Search has been clicked.');
-    window.location.replace("index2.html");
-}
+//localStorage
 
+function getSearchInput(searchItem) {
+    // e.preventDefault();
+    console.log(searchItem);
+}
